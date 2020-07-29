33d5f868f52bad71cf2ea7586a6a33c3e65257e3
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-07-29 11:44:49 +0700

    -On Progress

--- src/components/other/Cta.js
+++ src/components/other/Cta.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { FiChevronRight } from "react-icons/fi";
 
 export default class Cta extends Component {
-    render() {
-        return (
-            <section className="cta-wrapper">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-8">
-                            <h1>This Year <strong>55 Companies</strong> Started & They Are in Business With Us.</h1>
-                        </div>
-                        <div className="col-4 text-right">
-                            <Link to="/#" className="theme-button">Start Project <FiChevronRight className="icon" /></Link>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section className="cta-wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <h1>
+                Tahun ini <strong>60 Perusahaan</strong> telah berbisnis dengan
+                perusahaan kami
+              </h1>
+            </div>
+            <div className="col-4 text-right">
+              <Link to="/contact" className="theme-button">
+                Mulai Sekarang <FiChevronRight className="icon" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
--- src/components/portfolio/layananAK3Ucontent.js
+++ src/components/portfolio/layananAK3Ucontent.js
@@ -23,6 +23,114 @@ export default class PortfolioDetailsContent extends Component {
                 perundang undangan yang berlaku. Kegiatan ini merupakan
                 pelatihan resmi yang tersertifikasi oleh Kemnaker RI.
               </p>
+              <p>
+                Di Indonesia sendiri, K3 diatur dalam:
+                <br />
+                &nbsp;&nbsp;● &nbsp;UU No.1 Tahun 1970 tentang keselamatan kerja
+                <br />
+                &nbsp;&nbsp;● &nbsp;UU No.23 Tahun 1992 tentang kesehatan
+                <br />
+                &nbsp;&nbsp;● &nbsp;UU No.13 Tahun 2003 tentang ketenagakerjaan
+              </p>
+
+              <p>
+                Untuk melengkapi Undang-Undang di atas, pemerintah juga
+                mengeluarkan Peraturan Pemerintah (PP) hingga Keputusan Presiden
+                terkait dengan penyelenggaraan K3, yaitu:
+                <br />
+                &nbsp;&nbsp;● &nbsp;PP No.11 Tahun 1979 tentang keselamatan
+                kerja pada permunian serta pengelolaan minyak dan gas bumi
+                <br />
+                &nbsp;&nbsp;● &nbsp;PP No.7 Tahun 1973 tentang penggunaan,
+                peredaran, dan penyimpangan pestisida
+                <br />
+                &nbsp;&nbsp;● &nbsp;PP No.13 Tahun 1973 tentang pengaturan serta
+                pengawasan keselamatan kerja di bidang pertambangan
+                <br />
+                &nbsp;&nbsp;● &nbsp;Keputusan presiden No.22 Tahun 1993 mengenai
+                penyakit yang timbul akibat hubungan kerja
+              </p>
+              <h1>Manfaat AK3U</h1>
+              <p>
+                Manfaat dari sertifikasi Ahli K3 Umum meningkatkan kompetensi
+                diri merupakan salah satu cara untuk meningkatkan daya jual
+                individu dalam mendapatkan pekerjaan.
+              </p>
+              <p>
+                Manfaat lainnya yang dapat diperoleh oleh individu yang telah
+                memiliki pengalaman kerja sebelumnya adalah dalam menunjang
+                promosi jabatan di perusahaan sebelumnya atau modal untuk
+                mencari pengalaman yang lebih luas dengan jabatan yang berbeda
+                di perusahaan baru.
+              </p>
+              <p>
+                Jelas bahwa, mengikuti program pembinaan Ahli K3 Umum merupakan
+                investasi bagi setiap individu baik yang telah menjadi bagian
+                dari Praktisi K3 (HSE Profesional) maupun bagi individu yang
+                ingin menjadi bagian dari keluarga K3. Salah satu slogan yang
+                sering kita dengar adalah “Safety is Everyone Responsibility”
+                atau “Keselamatan merupakan tanggungjawab semua orang”. Slogan
+                ini mengingatkan kita bahwa K3 merupakan kebutuhan semua orang
+                sehingga tercapailah tujuan “Mulailah pekerjaan dengan semangat
+                dan pulang dengan selamat”
+              </p>
+              <p>
+                Setelah selesai mengikuti pembinaan dan lulus ujian, peserta
+                akan mengetahui tugas dan kewajiban dalam melaksanakan
+                persyaratan, sistem dan proses K3 di tempat kerja sesuai dengan
+                peraturan dan perundang-undangan yang berlaku.
+              </p>
+              <p>
+                ● &nbsp;Menjelaskan tugas, wewenang dan tanggung jawab AK3
+                <br />
+                ● &nbsp;Mengetahui hak pekerja dalam bidang K3 <br />
+                ● &nbsp;Menjelaskan kepada pengusaha bahwa upaya K3
+                menguntungkan bagi perusahaan
+                <br />
+                ● &nbsp;Mengetahui tujuan sistem manajemen K3 (SMK3)
+                <br />
+                ● &nbsp;Menjelaskan sistem pelaporan kecelakaan
+                <br />
+                ● &nbsp;Menganalisa kasus kecelakaan, mengetahui factor
+                penyebabnya, dan dapat menyiapkan laporan kecelakaan terhadap
+                pihak terkait
+                <br />
+                ● &nbsp;Mengenal P2K3, tugas, tanggung jawab, dan wewenang
+                organisasi ini
+                <br />● &nbsp;Mengetahui pembinaan dan pengawasan K3 di tingkat
+                perusahaan, Nasional & Internasional
+                <br />
+                ● &nbsp;Mengidentifikasi obyek pengawasan K3
+                <br />
+                ● &nbsp;Mengetahui persyaratan dan pemenuhan terhadap peraturan
+                perundangan di tempat kerja
+                <br />
+                ● &nbsp;Memahami persyaratan keselamatan dan kesehatan di tempat
+                kerja
+                <br />● Mengetahui proses audit dan ruang lingkupnya untuk
+                mengukur tingkat pencapaian
+              </p>
+              <h1>Persyaratan</h1>
+              <p>
+                Beberapa persyaratan dokumen yang harus dipenuhi bagi calon
+                peserta pelatihan ahli K3 umum adalah sebagai berikut :
+                <br />
+                1. Pendidikan terakhir minimal DIII
+                <br />
+                2. Berpengalaman kerja di bidang terkait minimal 1 tahun
+                <br />
+                3. Melampirkan foto copy ijazah terakhir
+                <br />
+                4. Melampirkan foto copy KTP
+                <br />
+                5. Melampirkan pas foto background merah ukuran 1,5cm x 2cm 4cm
+                x 6cm masing-masing 4 lembar
+                <br />
+                6. Melampirkan surat keterangan sehat dari klinik /RS terdekat
+                <br />
+                7. Melampirkan surat pengantar /keterangan /penunjukan dari
+                perusahaan
+              </p>
             </div>
           </div>
         </div>
